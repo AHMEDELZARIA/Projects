4015401c42041d8833a490bd2c1f97eb6ecf3ae6
AHMEDELZARIA <93144563+AHMEDELZARIA@users.noreply.github.com>
2021-12-20 14:26:06 -0500

    Add files via upload
    This is an adding game where it prompts the user with random addition questions and keeps count of the score. It also recognizes if an answer is wrong. Finally when the user is done, they can see their score at the end.

---
+++ Adder.java
@@ -0,0 +1,38 @@
+package ch8ex8;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Adder {
+    private int answer, score = 0;
+    
+    void createP(){
+        Random r = new Random();
+        int num1 = r.nextInt(20);
+        int num2 = r.nextInt(20);
+        answer = num1 + num2;
+        System.out.print("\n" + num1 + " + " + num2 + " = ");
+    }
+    void startG(){
+        Scanner input = new Scanner(System.in);
+        int guess = 0;
+            
+        while(guess != 999){
+        createP();
+        for(int tries = 0; tries < 3 && guess != 999; tries++){
+            guess = input.nextInt();
+            if(guess == answer){
+            if(tries == 0) score +=5;
+            if(tries == 1) score +=3;
+            if(tries == 2) score +=1;
+            break;
+        }
+        
+        if(guess != answer && guess != 999 && tries <2){
+            System.out.print("Wrong answer. Enter another answer: ");        
+        }
+        }
+        }
+        System.out.println("Your score is: " + score);
+    }
+    }
+
---
+++ Ch8Ex8.java
@@ -0,0 +1,7 @@
+package ch8ex8;
+public class Ch8Ex8 {
+public static void main(String[] args) {
+    Adder mathGame = new Adder();
+    mathGame.startG();
+}    
+}
