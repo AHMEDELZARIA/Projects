dfbcab0e77ca5793b181d192f8d405f3c406d970
AHMEDELZARIA <93144563+AHMEDELZARIA@users.noreply.github.com>
2021-12-20 15:03:53 -0500

    Add files via upload
    This is a program where it displays a starting icon and based on the scaling factor the user inputs, the icon will change in size.

---
+++ Assign13.java
@@ -0,0 +1,59 @@
+package assign13;
+import java.util.Scanner;
+public class Assign13 {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    
+    System.out.println("Here is the starting icon: ");
+    System.out.println("*X*");
+    System.out.println("X**");
+    System.out.println("**X");
+    System.out.print("Choose a scale factor: ");
+    int sFactor = input.nextInt();
+    System.out.println("Here is your scaled icon by " + sFactor + " times:");
+    scaling(sFactor);
+    
+    
+    }
+    public static void scaling(int sFactor){
+    
+    for (int i = 0; i < (sFactor); i++){
+        for (int j = 0; j < (sFactor); j++){
+        System.out.print("*");  
+        }
+        for (int j = 0; j < (sFactor); j++){
+        System.out.print("X");  
+        }
+        for (int j = 0; j < (sFactor); j++){
+        System.out.print("*");  
+        }
+    System.out.println();
+    }
+    for (int i = 0; i < (sFactor); i++){
+        for (int j = 0; j < (sFactor); j++){
+        System.out.print("X");  
+        }
+        for (int j = 0; j < (sFactor); j++){
+        System.out.print("*");  
+        }
+        for (int j = 0; j < (sFactor); j++){
+        System.out.print("*");  
+        }
+    System.out.println();
+    }
+for (int i = 0; i < (sFactor); i++){
+        for (int j = 0; j < (sFactor); j++){
+        System.out.print("*");  
+        }
+        for (int j = 0; j < (sFactor); j++){
+        System.out.print("*");  
+        }
+        for (int j = 0; j < (sFactor); j++){
+        System.out.print("X");  
+        }
+    System.out.println();
+    }
+    }
+}
+    
+
