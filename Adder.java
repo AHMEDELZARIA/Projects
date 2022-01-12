5003e692bb27dce827a3bfacf7417bfb79e7b063
Ahmed Elzaria <93144563+AHMEDELZARIA@users.noreply.github.com>
2022-01-12 15:05:12 -0500

    Update Adder.java

--- Adder.java
+++ Adder.java
@@ -3,21 +3,27 @@ package adder;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Adder {
+public class Adder 
+{
     private int answer, score = 0;
     
-    void createP(){
+    // Generates random addition question
+    void createP()
+    {
         Random r = new Random();
         int num1 = r.nextInt(20);
         int num2 = r.nextInt(20);
         answer = num1 + num2;
         System.out.print("\n" + num1 + " + " + num2 + " = ");
     }
-    void startG(){
+    void startG()
+    {
         Scanner input = new Scanner(System.in);
         int guess = 0;
-            
-        while(guess != 999){
+        
+        // Generates points based on number of tries, input of 999 ends game
+        while(guess != 999)
+        {
         createP();
         for(int tries = 0; tries < 3 && guess != 999; tries++){
             guess = input.nextInt();
@@ -28,12 +34,14 @@ public class Adder {
             break;
         }
         
-        if(guess != answer && guess != 999 && tries <2){
+        // Prints out try again message
+        if(guess != answer && guess != 999 && tries <2)
+        {
             System.out.print("Wrong answer. Enter another answer: ");        
         }
         }
         }
         System.out.println("Your score is: " + score);
     }
-    }
+}
 
