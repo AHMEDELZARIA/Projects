dee3e9f00a7d3dab5d5c7eb210201f7e348db2ff
Ahmed Elzaria <93144563+AHMEDELZARIA@users.noreply.github.com>
2022-01-11 20:59:52 -0500

    Update Ch6Ex18.java

--- Ch6Ex18.java
+++ Ch6Ex18.java
@@ -1,14 +1,27 @@
-package ch6ex18;
+// Generates a personalized monogram
+package monogram;
+
 import java.util.Scanner;
-public class Ch6Ex18 {
-public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    String name1, name2, name3;
-    System.out.print("Enter your fist name: ");
-    name1 = input.next();
-    System.out.print("Enter your middle initial: ");
-    name2 = input.next();
-    System.out.print("Enter your last name: ");
-    name3 = input.next();
-    System.out.println("Your monogram is: " + name1.substring(0,1).toLowerCase() + name3.substring(0,1).toUpperCase() + name2.substring(0,1).toLowerCase());
-}}
+
+public class Monogram {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String name1, name2, name3;
+        
+        // Prompts fdor first name
+        System.out.print("Enter your fist name: ");
+        name1 = input.next();
+        
+        // Prompts for middle name initial
+        System.out.print("Enter your middle initial: ");
+        name2 = input.next();
+        
+        // Prompts for last name
+        System.out.print("Enter your last name: ");
+        name3 = input.next();
+        
+        // Prints out monogram
+        System.out.println("Your monogram is: " + name1.substring(0, 1).toLowerCase() + name3.substring(0, 1).toUpperCase() + name2.substring(0, 1).toLowerCase());
+    }
+}
