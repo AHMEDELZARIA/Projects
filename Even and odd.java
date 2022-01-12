cb15f4c1631b12016b43943c731c7d1b44a383a1
Ahmed Elzaria <93144563+AHMEDELZARIA@users.noreply.github.com>
2022-01-11 20:24:26 -0500

    Update Even and odd.java

--- Even and odd.java
+++ Even and odd.java
@@ -6,22 +6,26 @@ import java.util.Random;
 public class EvenOdd {
 
     public static void main(String[] args) {
+        // Declare array of size 25 that will hold the 25 random numbers
         int nums[] = new int[25];
         Random r = new Random();
-
+        
+        // Generates 25 random numbers 0 - 100 and prints them out
         System.out.println("Numbers:");
         for (int c = 0; c < nums.length; c++) {
             nums[c] = r.nextInt(100);
             System.out.print(nums[c] + " ");
         }
-
+        
+        // Checks if even, if true, print them 
         System.out.println("\nEven:");
         for (int c = 0; c < nums.length; c++) {
             if (nums[c] % 2 == 0) {
                 System.out.print(nums[c] + " ");
             }
         }
-
+        
+        // Checks if odd, if true, print them
         System.out.println("\nOdd:");
         for (int c = 0; c < nums.length; c++) {
             if (nums[c] % 2 != 0) {
