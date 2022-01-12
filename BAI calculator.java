825a8f455c6df06dcf4e63bbe362457ad25d7b0e
Ahmed Elzaria <93144563+AHMEDELZARIA@users.noreply.github.com>
2022-01-11 20:06:33 -0500

    Update BAI calculator.java

--- BAI calculator.java
+++ BAI calculator.java
@@ -3,7 +3,7 @@ package baicalculator;
 
 import java.util.Scanner;
 
-public class baicalculator {
+public class Baicalculator {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
