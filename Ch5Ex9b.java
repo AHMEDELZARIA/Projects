b8642699ac87274c52d76ae2df36964d73155519
Ahmed Elzaria <93144563+AHMEDELZARIA@users.noreply.github.com>
2022-01-11 20:53:20 -0500

    Update Ch5Ex9b.java

--- Ch5Ex9b.java
+++ Ch5Ex9b.java
@@ -1,20 +1,34 @@
-package ch5ex9b;
+// Number guessing game between user and computer
+package guess;
+
 import java.util.*;
-public class Ch5Ex9b {
-public static void main(String[] args) {
-Scanner input = new Scanner(System.in);
-int guess;
-Random r = new Random();
-int number = r.nextInt(20)+1;
-System.out.print("Enter a number between 1 and 20: ");
-guess = input.nextInt();
-System.out.println("Computer's number: " + number);
-System.out.println("Player's number: " + guess);
-if (number == guess) {
-    System.out.println("You got it!"); 
-}
-if (number != guess) {
-    System.out.println("Better luck next time.");
-}
-}    
+
+public class Guess {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int guess;
+        
+        // Generates a random number between 1 - 20
+        Random r = new Random();
+        int number = r.nextInt(20) + 1;
+        
+        // Prompts user for guess
+        System.out.print("Enter a number between 1 and 20: ");
+        guess = input.nextInt();
+        
+        // Prints out computer's and player's numbers
+        System.out.println("Computer's number: " + number);
+        System.out.println("Player's number: " + guess);
+        
+        // Checks for win
+        if (number == guess) 
+        {
+            System.out.println("You got it!");
+        }
+        if (number != guess) 
+        {
+            System.out.println("Better luck next time.");
+        }
+    }
 }
