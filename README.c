7eb6d82a4299eb780b961aa563521aee607d4001
Ahmed Elzaria <93144563+AHMEDELZARIA@users.noreply.github.com>
2022-07-17 20:27:14 -0400

    Update README.c

--- README.c
+++ README.c
@@ -3,5 +3,6 @@
 int main() 
 { 
     printf("Hello there! Here are some projects I have created"); 
+    printf("You will also find some problems I have solved and upgraded);
     printf("Currently all coded in JAVA, C, and ELM"); 
 }
